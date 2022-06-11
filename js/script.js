70e91f6813f016763243feff5865d3aa61fd410b
Youssef Mohamed <99333110+YoussefMohamed256374@users.noreply.github.com>
2022-06-11 18:27:50 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let navbar = document.querySelector('.header .navbar ul');
+let menu = document.querySelector('#menu-btn');
+
+menu.onclick = () =>{
+	menu.classList.toggle("fa-times");
+	navbar.classList.toggle("active");
+}
+window.onscroll = () =>{
+	menu.classList.remove("fa-times");
+	navbar.classList.remove("active");
+}
+
